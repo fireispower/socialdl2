c2a56c88ca0ff3aaaab6a83c06cf4bda3c880441
Krxuv <141473626+Krxuv@users.noreply.github.com>
2023-12-27 19:44:37 +0800

    update BotController.js

--- server/controllers/BotController.js
+++ server/controllers/BotController.js
@@ -22,27 +22,27 @@ class BotController {
   async handle() {
     try {
       if (constants.COMMAND_SHORT_REGEX.test(this.text)) {
-        const shortService = new ShortService(this.bot, this.id, this.text);
+        const shortService = new ShortService(this.bot, this.body.message.chat.id, this.text);
 
         await shortService.short();
       } else if (constants.COMMAND_SLUG_REGEX.test(this.text)) {
-        const slugService = new SlugService(this.bot, this.id, this.text);
+        const slugService = new SlugService(this.bot, this.body.message.chat.id, this.text);
 
         await slugService.slug();
       } else if (constants.COMMAND_HEX_REGEX.test(this.text)) {
-        const hexService = new HexService(this.bot, this.id, this.text);
+        const hexService = new HexService(this.bot, this.body.message.chat.id, this.text);
 
         await hexService.hex();
       } else if (constants.COMMAND_MD5_REGEX.test(this.text)) {
-        const mD5Service = new MD5Service(this.bot, this.id, this.text);
+        const mD5Service = new MD5Service(this.bot, this.body.message.chat.id, this.text);
 
         await mD5Service.md5();
       } else if (constants.COMMAND_BCRYPT_REGEX.test(this.text)) {
-        const bcryptService = new BcryptService(this.bot, this.id, this.text);
+        const bcryptService = new BcryptService(this.bot, this.body.message.chat.id, this.text);
 
         await bcryptService.bcrypt();
       } else if (constants.COMMAND_BINARY_REGEX.test(this.text)) {
-        const binaryService = new BinaryService(this.bot, this.id, this.text);
+        const binaryService = new BinaryService(this.bot, this.body.message.chat.id, this.text);
 
         await binaryService.binary();
       } else if (/(but|button)/.test(this.text)) {
@@ -55,8 +55,8 @@ class BotController {
                   ]
               })
           }
-          await this.bot.sendMessage(this.id, "Tes Button", butt)
-          await this.bot.sendMessage(this.id, require("util").format(this.body))
+          await this.bot.sendMessage(this.body.message.chat.id, "Tes Button", butt)
+          await this.bot.sendMessage(this.body.message.chat.id, require("util").format(this.body))
       }
       switch (this.body.callback_query.data) {
           case "cat": {
@@ -68,12 +68,12 @@ class BotController {
         case constants.COMMAND_START:
         case constants.COMMAND_COMMANDS:
         case constants.COMMAND_HELP: {
-          const helpService = new HelpService(this.bot, this.id);
+          const helpService = new HelpService(this.bot, this.body.message.chat.id);
 
           await helpService.help();
           break;
         } case constants.COMMAND_UUID: {
-          const uUIDService = new UUIDService(this.bot, this.id, this.text);
+          const uUIDService = new UUIDService(this.bot, this.body.message.chat.id, this.text);
 
           await uUIDService.uuid();
           break;
